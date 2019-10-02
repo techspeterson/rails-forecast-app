36f80367ae7143ec983c1db4c2c274d6951727c4
techspeterson <tpeterson.engsci@gmail.com>
2019-10-02 11:55:10 +1000

    view fixes

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,10 +1,11 @@
 class CitiesController < ApplicationController
   before_action :set_city, only: [:show, :edit, :update, :destroy]
+  before_action :set_state
 
   # GET /cities
   # GET /cities.json
   def index
-    @cities = City.all
+    @cities = City.order('name').select {|city| city.state_id == @state.id}
   end
 
   # GET /cities/1
@@ -28,7 +29,7 @@ class CitiesController < ApplicationController
 
     respond_to do |format|
       if @city.save
-        format.html { redirect_to @city, notice: 'City was successfully created.' }
+        format.html { redirect_to state_city_path(@state, @city), notice: 'City was successfully created.' }
         format.json { render :show, status: :created, location: @city }
       else
         format.html { render :new }
@@ -42,7 +43,7 @@ class CitiesController < ApplicationController
   def update
     respond_to do |format|
       if @city.update(city_params)
-        format.html { redirect_to @city, notice: 'City was successfully updated.' }
+        format.html { redirect_to state_city_path(@state, @city), notice: 'City was successfully updated.' }
         format.json { render :show, status: :ok, location: @city }
       else
         format.html { render :edit }
@@ -56,13 +57,17 @@ class CitiesController < ApplicationController
   def destroy
     @city.destroy
     respond_to do |format|
-      format.html { redirect_to cities_url, notice: 'City was successfully destroyed.' }
+      format.html { redirect_to state_cities_path(@state), notice: 'City was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
+    def set_state
+      @state = State.find(params[:state_id])
+    end
+
     def set_city
       @city = City.find(params[:id])
     end
